21a8d3494d2edf1144621d9714880b561f6717aa
mario-platt <37125349+mario-platt@users.noreply.github.com>
2020-07-02 12:55:45 +0100

    Update cookies.rb
    added tags for ISO27001, ASVS and renamed the tests to what they actually do

--- controls/cookies.rb
+++ controls/cookies.rb
@@ -9,19 +9,23 @@ title 'cookies-policy'
 #end
 
 # you add controls here
-control 'ASVS-14.4.4' do                        # A unique ID for this control
+control 'HTTP Header - Content Type Options' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'Content type Options = no sniff'
   desc 'All responses should contain X-Content-Type-Options=nosniff'
+  tag 'ASVS-14.4.4'
+  tag 'ISO27001-14.1.2'
   describe http('http://192.168.0.6:3000') do                  # The actual test
     its('headers.x-content-type-options') { should cmp 'nosniff' } 
   end
 end
 
-control 'ASVS-14.4.7' do                        # A unique ID for this control
+control 'HTTP Headers - X-Frame Options or CSP' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'X Frame Options or Content Security policy set'             # A human-readable title
   desc 'All responses should include X Frame Options or Content Security Policy'
+  tag 'ASVS-14.4.7'
+  tag 'ISO27001-14.1.2'
   describe http('http://192.168.0.6:3000').headers do                  # The actual test
     its('x-frame-options') { should cmp 'SAMEORIGIN' }
   end
@@ -29,10 +33,12 @@ control 'ASVS-14.4.7' do                        # A unique ID for this control
 end
 
 
-control 'ASVS-14.4.1' do                        # A unique ID for this control
+control 'HTTP Header - Content type' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'Safe character set'             # A human-readable title
   desc 'HTTP response contains content type header with safe character set'
+  tag 'ASVS-14.4.1'
+  tag 'ISO27001-14.1.2'
   describe http('http://192.168.0.6:3000') do                  # The actual test
     its('headers.Content-type') { should cmp 'text/html; charset=utf-8' }
   end
